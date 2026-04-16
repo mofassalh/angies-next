543fbf30574e78236ab7ecf3ef99fdf1dd3d836d
mofassal hossain <mofassalhossain@193-183-133-237.ip.obe.net>
2026-04-17 01:26:09 +0200

    feat: /admin redirect to angies-admin.vercel.app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://angies-admin.vercel.app',
+        permanent: false,
+      },
+      {
+        source: '/admin/:path*',
+        destination: 'https://angies-admin.vercel.app/admin/:path*',
+        permanent: false,
+      },
+    ]
+  },
 };
-
 export default nextConfig;
