2115b97b5031bf2d4b72581d3b0927c4940896cf
mofassalh <mofassalh@gmail.com>
2026-06-03 00:38:14 +0200

    fix: supabase image domain

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['egyxvzjfqnpcfdnwusxn.supabase.co'],
+  },
   async redirects() {
     return [
       {
